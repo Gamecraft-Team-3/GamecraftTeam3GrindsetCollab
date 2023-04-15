50cd88cd25b4827d6a5e877cdf6c9b95701f7f48
AlmityTuhm <driggsthomas@gmail.com>
2023-04-14 20:25:35 -0600

    Added Enemy Jazz

---
+++ Assets/Scripts/EnemyScripts/EnemyBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private float speed = 0;
+    [SerializeField] Animator anim = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
