c19b5273fd88a1d7390a1ea0dc0b77f3e0b65c43
Enoch-Nielsen <93846909+Enoch-Nielsen@users.noreply.github.com>
2023-04-15 15:11:26 -0600

    Player Shoot and LASERS

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -1,19 +1,65 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Player
 {
     public class PlayerCombat : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        // [Header("Components")] 
+        // [SerializeField] private PlayerMeshController meshController;
         
+        [Header("Objects")] 
+        [SerializeField] private GameObject bulletPrefab;
+        [SerializeField] private Transform meshTransform;
+
+        [Header("Values")] 
+        [SerializeField] private bool canShoot;
+        [SerializeField] private bool isShootPressed;
+        
+        [Header("Fields")]
+        [SerializeField] private float fireRate;
+        [SerializeField] private bool fullAuto;
+
+        private void Start()
+        {
+            PlayerInputController.Instance.OnShootActionPerformed += ShootPressed;
+            PlayerInputController.Instance.OnShootActionReleased += ShootReleased;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            if (fullAuto && canShoot && isShootPressed)
+                Shoot();
+        }
+
+        private void ShootPressed(object sender, EventArgs e)
+        {
+            isShootPressed = true;
+
+            if (!fullAuto)
+                Shoot();
+        }
+
+        private void ShootReleased(object sender, EventArgs e)
+        {
+            isShootPressed = false;
+        }
+
+        private void Shoot()
+        {
+            GameObject bulletInstance = Instantiate(bulletPrefab, meshTransform.position, Quaternion.identity);
+            bulletInstance.transform.forward = meshTransform.forward;
+
+            canShoot = false;
+            StartCoroutine(SetCanShoot(fireRate));
+        }
+
+        private IEnumerator SetCanShoot(float time)
+        {
+            yield return new WaitForSeconds(time);
+
+            canShoot = true;
         }
     }
 }
