8c3d169b795e8bd65bf858145c6c9f7941c0ca38
AlmityTuhm <driggsthomas@gmail.com>
2023-04-14 20:53:01 -0600

    Wak

--- Assets/Scripts/EnemyScripts/EnemyBaseFSM.cs
+++ Assets/Scripts/EnemyScripts/EnemyBaseFSM.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class EnemyBaseFSM : StateMachineBehaviour
 {
+    protected float distance = 0;
+    protected EnemyBehavior enemyBehavior = null;
+    protected List<Vector3> wayPoints = null;
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        distance = animator.GetFloat("DistanceFromPlayer");
+        Debug.Log(distance);
+        enemyBehavior = animator.gameObject.GetComponent<EnemyBehavior>();
+        wayPoints = enemyBehavior.GetWayPoints();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+
     {
-        
+        distance = enemyBehavior.GetPlayerDistance();
+        animator.SetFloat("DistanceFromPlayer", distance);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
--- Assets/Scripts/EnemyScripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyScripts/EnemyBehavior.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
     [SerializeField] private GameObject player = null;
-    [SerializeField] private float speed = 0;
+    [SerializeField] private float speed, distanceToPlayer;
     [SerializeField] Animator anim = null;
+    [SerializeField] private NavMeshAgent agent = null;
+    [SerializeField] private List<Vector3> wayPoints = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,25 @@ public class EnemyBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    #region Getter Functions
+    //Returns the distacne from the player
+    public float GetPlayerDistance()
+    {
+        return distanceToPlayer;
+    }
+
+    //Returns the way points the enemy will be patroling
+    public List<Vector3> GetWayPoints()
+    {
+        return wayPoints;
+    }
+
+    //Returns the Nav Mesh Agent
+    public NavMeshAgent GetAgent()
+    {
+        return agent;
+    }
+    #endregion
 }
