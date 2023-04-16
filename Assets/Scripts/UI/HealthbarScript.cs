fdc4ca021a25fac2eb6ad304a5ac50b5a198c144
Fetoose <112012621+Fetoose@users.noreply.github.com>
2023-04-15 21:54:52 -0600

    Working on connecting Player to HUD

--- Assets/Scripts/UI/HealthbarScript.cs
+++ Assets/Scripts/UI/HealthbarScript.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class HealthbarScript : MonoBehaviour
 {
-    private int health = 3;
-    //[SerializeField] public PlayerHUD hud;
+    [SerializeField] private Image heart1;
+    [SerializeField] private Image heart2;
+    [SerializeField] private Image heart3;
+    [SerializeField] private Sprite heartAlive;
+    [SerializeField] private Sprite heartDead;
+
+    public void UpdateHealth(int health)
+    {
+        switch(health)
+        {
+            case 2:
+                {
+                    heart3.sprite = heartDead;
+                }
+            case 1:
+                {
+                    heart2.sprite = heartDead;
+                }
+            case 0:
+                {
+                    heart1.sprite = heartDead;
+                }
+        }
+    }
 }
